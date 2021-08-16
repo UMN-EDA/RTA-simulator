f68741ea290b46e1e3b00434d39bf676afb8eee1
VidyaChhabria <chhab011@umn.edu>
2021-08-16 04:17:35 -0500

    Add licences to source code

--- src/ThermalAnalyzer.py
+++ src/ThermalAnalyzer.py
@@ -1,3 +1,30 @@
+"""
+BSD 3-Clause License
+Copyright (c) 2021, The Regents of the University of Minnesota
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+# Author: VidyaChhabria
+ 
 import numpy as np
 import sys
 import argparse
@@ -39,9 +66,9 @@ class ThermalAnalyzer:
         self.parserVisualize.error("--resolution is a required argument with --emissivity")
         return 
       self.visualizer.visualizeEmmissivity(self.args.npzFile, self.args.R)
-    if (self.args.lvt is not None or 
-        self.args.lvh is not None or
-        self.args.lvw is not None ):
+    if (self.args.lvt is not False or 
+        self.args.lvh is not False or
+        self.args.lvw is not False ):
       if self.args.solFile is None:
         self.parserVisualize.error('''--solution is a required argument with -lvt,
                                       -lvh, -lvw''')
@@ -166,7 +193,7 @@ class ThermalAnalyzer:
 
     #### Visualize options
     parserVisualize = subparsers.add_parser("visualize", 
-              description="GDS visualizetion",
+              description="GDS visualization",
               help=''' Set of suboptions for visualizing the inputs/outputs ''')
     group_emmis = parserVisualize.add_argument_group('Emmissivty',
                           'Necessary arguments to generate emissivity plots')
